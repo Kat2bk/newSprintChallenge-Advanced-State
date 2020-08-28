88f2b6396d169ca6f38f3f66bd12b03c3ebe73a2
unknown <Katherine2bk@gmail.com>
2020-08-28 18:44:54 -0500

    finished majority of reducer, must begin work on actions to import into reducer

---
+++ smurfs/src/reducer/index.js
@@ -0,0 +1,32 @@
+
+const initialState = {
+    smurfs: [],
+    error: "",
+    isFetching: false
+}
+
+// need start, success, failure
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCHING_START:
+            return {
+                ...state,
+                isFetching: true
+            };
+        case FETCHING_SUCCESS:
+            return {
+                ...state,
+                smurfs: action.payload,
+                isFetching: false
+            };
+        case FETCHING_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload
+            };
+        default:
+            return state;
+    }
+}

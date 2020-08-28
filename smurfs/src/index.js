3e8d55a32c62f2a84b25dcf47d1654ef7e69b1be
unknown <Katherine2bk@gmail.com>
2020-08-28 18:25:39 -0500

    downloaded necessary Redux, React-Redux libraries... set up store in index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+
+const store = createStore(applyMiddleware(thunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));

4c4bb1ae515e6fe89c8619aeada0dd47fb9c1cfc
unknown <Katherine2bk@gmail.com>
2020-08-28 19:05:32 -0500

    it wasn't working because I did not build a Smurf component to call the data and map the Props to state to connect it to the data, data is now displaying in console

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,13 +4,15 @@ export const FETCHING_START = "FETCHING_START";
 export const FETCHING_SUCCESS = "FETCHING_SUCCESS";
 export const FETCHING_FAILURE = "FETCHING_FAILURE";
 
-export const gettingSmurfs = (dispatch) => {
+export const gettingSmurfs = () => (dispatch) => {
     dispatch({type: FETCHING_START});
     axios.get("http://localhost:3333/smurfs")
     .then(response => {
         console.log(response);
+        dispatch({type: FETCHING_SUCCESS, payload: response})
     })
     .catch(error => {
         console.log("unable to get data", error);
+        dispatch({type: FETCHING_FAILURE, payload: error})
     })
 }
---
+++ smurfs/src/components/SmurfProfile.js
@@ -0,0 +1,29 @@
+import React, {useEffect} from "react";
+import {gettingSmurfs} from "../actions";
+import {connect} from "react-redux";
+
+const SmurfProfile = (props) => {
+console.log(props)
+    const getData = props.gettingSmurfs;
+
+    useEffect(() => {
+        getData();
+    }, [getData])
+
+
+    return (
+        <div>
+        <div className="smurf-container">
+        </div>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        smurfs: state.smurfs,
+        error: state.error
+    }
+}
+
+export default connect(mapStateToProps, {gettingSmurfs})(SmurfProfile);

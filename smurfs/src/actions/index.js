860596eb12f8f89c33b0d3236e6f5f816b9f35f5
unknown <Katherine2bk@gmail.com>
2020-08-28 20:32:34 -0500

    added delete action, importing into reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -8,6 +8,9 @@ export const POST_START = "POST_START";
 export const POST_SUCCESS = "POST_SUCCESS";
 export const POST_FAILURE = "POST_FAILURE";
 
+export const DELETE_SUCCESS = "DELETE_SUCCESS";
+export const DELETE_FAILURE = "DELETE_FAILURE";
+
 export const gettingSmurfs = () => (dispatch) => {
     dispatch({type: FETCHING_START});
     axios.get("http://localhost:3333/smurfs")
@@ -31,5 +34,17 @@ export const addSmurf = (newSmurf) => (dispatch) => {
     .catch(error => {
         console.log("unable to post", error);
         dispatch({type: POST_FAILURE, payload: error});
+    })
+}
+
+export const deleteSmurf = (id) => (dispatch) => {
+    axios.delete(`http://localhost:3333/smurfs/${id}`)
+    .then(response => {
+        console.log("deleting data", response);
+        dispatch({type: DELETE_SUCCESS, payload: response.data});
+    })
+    .catch(error => {
+        console.log("unable to delete", error);
+        dispatch({type: DELETE_FAILURE, payload: error});
     })
 }
